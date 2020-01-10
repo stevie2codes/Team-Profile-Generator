8b9e61ebd408b5037fd347fdef711bff184963ac
stevie2codes <stevie2codes@gmail.com>
2020-01-10 14:18:26 -0600

    added build_team file and added first manager prompt

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const Manager = require("./lib/Manager");
+const BuildTeam = require('./lib/build_team');
+
+BuildTeam();
 
 //manager prompt//
-function promptManager() {
-    return inquirer.prompt([
-        {
-        type: "input",
-        name: "name",
-        message: "What is your name"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "what is your email"
-    }
+// function promptManager() {
+//     return inquirer.prompt([
+//         {
+//         type: "input",
+//         name: "name",
+//         message: "What is your name"
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is your id?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "what is your email"
+//     }
 
-   ])
+//    ])
    
-};
+// };
 
-async function init () { 
-    const choiceData = await promptManager();
-    const { choices } = choiceData;
-//   console.log(choiceData.status);
-    if(choiceData.status === "engineer"){
-       console.log("successsss!");
-}
-}
-init();
+// async function init () { 
+//     const choiceData = await promptManager();
+//     const { choices } = choiceData;
+// //   console.log(choiceData.status);
+//     if(choiceData.status === "engineer"){
+//        console.log("successsss!");
+// }
+// }
+// init();
