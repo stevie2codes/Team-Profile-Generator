a8056591bdfc38798a9f75d2870ac6c1d1b77575
stevie2codes <stevie2codes@gmail.com>
2020-01-10 18:10:00 -0600

    exported an HTML template function to build sheet and got the file to write. Now need to setup variables for proper input

--- app.js
+++ app.js
@@ -1,39 +1,4 @@
-const inquirer = require("inquirer");
-const fs = require('fs');
-const Manager = require("./lib/Manager");
 const managerPrompt= require('./lib/build_team');
 
 managerPrompt();
 
-//manager prompt//
-// function promptManager() {
-//     return inquirer.prompt([
-//         {
-//         type: "input",
-//         name: "name",
-//         message: "What is your name"
-//     },
-//     {
-//         type: "input",
-//         name: "id",
-//         message: "What is your id?"
-//     },
-//     {
-//         type: "input",
-//         name: "email",
-//         message: "what is your email"
-//     }
-
-//    ])
-   
-// };
-
-// async function init () { 
-//     const choiceData = await promptManager();
-//     const { choices } = choiceData;
-// //   console.log(choiceData.status);
-//     if(choiceData.status === "engineer"){
-//        console.log("successsss!");
-// }
-// }
-// init();
